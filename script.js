4cedacf9d49a8414bb07e6ea6477a5e6b7cf4450
jhondev <125105950+Lea1dev@users.noreply.github.com>
2023-03-15 17:45:01 -0300

    conclusão do código

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-function mudarCor(){
-    roboAzul = document.getElementById('roboAzul');
-    roboAmarelo = document.getElementById('roboAmarelo');
-    roboBranco = document.getElementById('roboBranco');
-    roboPreto = document.getElementById('roboPreto');
-    roboRosa = document.getElementById('roboRosa');
-    roboVermelho = document.getElementById('roboVermelho');
-    imagens = [roboAzul, roboAmarelo, roboBranco, roboPreto, roboRosa, roboVermelho];
-    
+const imagens = ["img/robotron-azul.png", "img/robotron-amarelo.png", "img/robotron-branco.png", "img/robotron-preto.png", "img/robotron-rosa.png", "img/robotron-vermelho.png"];
+function proximaCor(){
+    var imagemAtual = document.getElementById('roboDefault');
+    switch(imagemAtual.getAttribute("src")){
+        case "img/robotron-azul.png":
+        case "img/robotron.png":
+            contador = 1;
+            imagemAtual.src = imagens[1];
+            break;
+        case "img/robotron-amarelo.png":
+            contador += 1;
+            imagemAtual.src = imagens[2];
+            break;
+        case "img/robotron-branco.png":
+            contador += 1;
+            imagemAtual.src = imagens[3];
+            break;
+        case "img/robotron-preto.png":
+            contador += 1;
+            imagemAtual.src = imagens[4];
+            break;
+        case "img/robotron-rosa.png":
+            contador += 1;
+            imagemAtual.src = imagens[5];
+            break;
+    }
+}
+
+function voltarCor(){
+    var imagemAtual = document.getElementById('roboDefault');
+    switch(imagemAtual.getAttribute("src")){
+        case "img/robotron-amarelo.png":
+            contador -= 1;
+            imagemAtual.src = imagens[0];
+            break;
+        case "img/robotron-branco.png":
+            contador -= 1;
+            imagemAtual.src = imagens[1];
+            break;
+        case "img/robotron-preto.png":
+            contador -= 1;
+            imagemAtual.src = imagens[2];
+            break;
+        case "img/robotron-rosa.png":
+            contador -= 1;
+            imagemAtual.src = imagens[3];
+            break;
+        case "img/robotron-vermelho.png":
+            contador -= 1;
+            imagemAtual.src = imagens[4];
+            break;
+    }
 }
